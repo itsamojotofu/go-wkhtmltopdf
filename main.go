1b4e761aea8521bd72ce5f7a85da7836f54d2058
sota <112251582+itsamojotofu@users.noreply.github.com>
2024-02-16 13:34:02 +0900

    test with html test

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
 	"log"
+	"strings"
 )
 
 func main() {
@@ -12,9 +13,17 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	url := "https://google.com/"
+	html := `
+    <html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+    <h1>契約締結時交付書面</h1>
+    </body>
+    </html>`
 
-	pdfg.AddPage(wkhtmltopdf.NewPage(url))
+	pdfg.AddPage(wkhtmltopdf.NewPageReader(strings.NewReader(html)))
 
 	// PDF作成
 	err = pdfg.Create()
@@ -23,7 +32,7 @@ func main() {
 	}
 
 	// 出力
-	err = pdfg.WriteFile("./google.pdf")
+	err = pdfg.WriteFile("./test.pdf")
 	if err != nil {
 		log.Fatal(err)
 	}
